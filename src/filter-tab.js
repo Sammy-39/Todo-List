cb954c43b17403c0c42c181e76224b1347bb2c81
sammy <sammy@others>
2021-02-05 17:15:09 +0530

    responsive update

--- src/filter-tab.js
+++ src/filter-tab.js
@@ -1,12 +1,12 @@
 
 const FilterTab = (props) =>{
     return(
-        <div className='filter mt-4 mb-1 col-12 col-sm-11 col-md-9 col-lg-6 col-xl-5'>
-            <h6 className="mr-2"> Count: {props.count} </h6>
-            <button type="button" className="btn btn-white mr-2 mb-1" onClick={props.all} > All </button>
-            <button type="button" className="btn btn-white mr-2 mb-1" onClick={props.active} > Active </button>
-            <button type="button" className="btn btn-white mr-2 mb-1" onClick={props.completed} > Completed </button>
-            <button type="button" className="btn btn-white mb-1" onClick={props.clearCompleted} > Clear Completed </button>
+        <div className='filter mt-4 mb-1 col-10 col-sm-9 col-md-7 col-lg-5 col-xl-4'>
+            <h6 className="mr-2"> {props.count} items </h6>
+            <button type="button" className="btn btn-white mr-1 mb-1" onClick={props.all} > All </button>
+            <button type="button" className="btn btn-white mr-1 mb-1" onClick={props.active} > Active </button>
+            <button type="button" className="btn btn-white mr-1 mb-1" onClick={props.completed} > Done </button>
+            <button type="button" className="btn btn-white mb-1" onClick={props.clearCompleted} > Clear Done </button>
         </div>
     )
 }
