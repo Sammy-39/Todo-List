4a82d4e9c7b1cf54dd01f26be9dff378cb0fbed9
sammy <sammy@others>
2021-02-05 17:25:57 +0530

    Commit-5

--- src/filter-tab.js
+++ src/filter-tab.js
@@ -1,7 +1,7 @@
 
 const FilterTab = (props) =>{
     return(
-        <div className='filter mt-4 mb-1 col-10 col-sm-9 col-md-7 col-lg-5 col-xl-4'>
+        <div className='filter mt-4 mb-1 col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5'>
             <h6 className="mr-2"> {props.count} items </h6>
             <button type="button" className="btn btn-white mr-1 mb-1" onClick={props.all} > All </button>
             <button type="button" className="btn btn-white mr-1 mb-1" onClick={props.active} > Active </button>
